068827222ca60ce61733bec466dd153c678e95a7
kaelyns <kaelyn.suh@gmail.com>
2016-12-09 18:07:44 -0500

    Set up smtp settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         :confirmable
 
   validates :fullname, presence: true, length: {maximum: 50}
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +53,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: 'kaelyn.suh@gmail.com',
+    password: 'pubkey-52eb4d10394114759224b4458b17aa1f',
+    authentication: 'login'
+  }
+
 end
