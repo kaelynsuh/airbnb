71ccf5381a6aa4c8ec7f4060b346e9f7bb936d22
kaelyns <kaelyn.suh@gmail.com>
2017-01-06 18:23:01 -0500

    Change toastr validations to new helper style and put placeholder img for near by rooms

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,5 +3,7 @@ class Photo < ApplicationRecord
   has_many :photos
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates_with AttachmentContentTypeValidator, attributes: :image, content_type: /\Aimage\/.*\z/
+
 end
